67d7be074d88397c1d548c0506c26ca3c59139f6
Chinmay <chinmay11.moghe@gmail.com>
2022-04-24 11:33:34 +0530

    Add comments for issues

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,9 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [
     react(),
+    /*fix for ffmpeg - https://github.com/chaosprint/vite-plugin-cross-origin-isolation
+     *issue - https://github.com/ffmpegwasm/ffmpeg.wasm/issues/263
+     */
     {
       name: "configure-response-headers",
       configureServer: (server) => {
